7f76095bcb68f1eac4021bc2786f5e186deb03fb
chezmarcbrown <marc@chezbrown.org>
2023-10-14 14:45:32 -0800

    feedback; see email for details.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,22 +9,23 @@ from django.contrib import messages
 
 from . import util
 
+markdowner = Markdown()
 
-def convert_md_to_html(title):
-     content = util.get_entry(title)
-     markdowner = Markdown()
-     if content == None:
-          return None
-     else:
-        return markdowner.convert(content)
+# def convert_md_to_html(title):
+#      content = util.get_entry(title)
+#      markdowner = Markdown()
+#      if content == None:
+#           return None
+#      else:
+#         return markdowner.convert(content)
 
 def entry(request, title):
-    html_content = convert_md_to_html(title)
-    if html_content ==None:
+    if util.get_entry(title) == None:
         return render(request, "encyclopedia/error.html",{
             "message":"This entry does not exit"
         })
     else:
+        html_content = markdowner.convert(util.get_entry(title))
         return render(request, "encyclopedia/entry.html",{
             "title":title,
             "content":html_content
@@ -32,23 +33,18 @@ def entry(request, title):
 
 
 def search(request):
-    if request.method == "POST":
-       entry_search =  request.POST['q']
-       html_content = convert_md_to_html(entry_search)
-       if html_content is not None:
-           return render(request, "encyclopedia/entry.html", {
-               "title":entry_search,
-               "content":html_content      
-                              })               
-       else:
-           allEntries = util.list_entries()
-           recommendation = []  
-           for entry in allEntries:
-               if entry_search.lower() in entry.lower():
-                   recommendation.append(entry)
-                   return render(request,"encyclopedia/search.html",{
-                      "recommendation":recommendation
-                   })          
+    entry_search =  request.GET.get('q')
+    if util.get_entry(entry_search) is not None:
+        return redirect('entry', title=entry_search)
+    else:
+        allEntries = util.list_entries()
+        recommendation = []  
+        for entry in allEntries:
+            if entry_search.lower() in entry.lower():
+                recommendation.append(entry)
+        return render(request,"encyclopedia/search.html",{
+            "recommendation":recommendation
+        })          
 
 
 
@@ -63,7 +59,7 @@ def index(request):
 
 
  
-def new_page(request):
+def new_page0(request):
     if request.method == "GET":
             return render(request,"encyclopedia/new.html")
     else:
@@ -81,14 +77,24 @@ def new_page(request):
                 "title":title,
                 "content":html_content
             })
-  
+            
+def new_page(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        if util.get_entry(title) is not None:
+            return render(request, "encyclopedia/new.html", {
+                "error_message": "Entry page already exists"
+            })
+        util.save_entry(title, content)
+        return redirect('entry', title)
+    else: 
+        return render(request,"encyclopedia/new.html")
+
+    
         
 def randompage(request):
     allEntries = util.list_entries()
     randompage_entry = random.choice(allEntries)
-    html_content = convert_md_to_html(randompage_entry)
-    return render(request, "encyclopedia/entry.html", {
-       "title":randompage_entry,
-       "content":html_content
-    })
+    return redirect('entry', title=randompage_entry)
      
